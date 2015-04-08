6c5bc0579b7b911f46d2167f6f7bac888d9c2828
kmadel <kmadel@mac.com>
2015-04-08 10:38:32 -0400

    Update flow.groovy

--- flow.groovy
+++ flow.groovy
@@ -1,5 +1,5 @@
 node('slave') {
-    git url: '/var/lib/jenkins/workflow-plugin-pipeline-demo'
+    git url: 'https://github.com/kmadel/workflow-plugin-pipeline-demo.git'
     env.PATH="${tool 'Maven 3.x'}/bin:${env.PATH}"
     stage 'Dev'
     sh 'mvn -o clean package'
@@ -8,45 +8,23 @@ node('slave') {
     stage 'QA'
 
     parallel(longerTests: {
-        runWithServer {url ->
-            sh "mvn -o -f sometests/pom.xml test -Durl=${url} -Dduration=30"
-        }
+        sh "mvn -o -f sometests/pom.xml test -Durl=${devHost} -Dduration=30"
     }, quickerTests: {
-        runWithServer {url ->
-            sh "mvn -o -f sometests/pom.xml test -Durl=${url} -Dduration=20"
-        }
+        sh "mvn -o -f sometests/pom.xml test -Durl=${devHost} -Dduration=20"
     })
     stage name: 'Staging', concurrency: 1
-    sh "mvn cargo:redeploy -Dhost=stage.jetty.docker"
+    sh "mvn cargo:redeploy -Dhost=${stageHost}"
 }
 
-input message: "Does http://stage.jetty.docker look good?"
+input message: "Does http://${stageHost}:8080 look good?"
 try {
     checkpoint('Before production')
 } catch (NoSuchMethodError _) {
     echo 'Checkpoint feature available in Jenkins Enterprise by CloudBees.'
 }
 stage name: 'Production', concurrency: 1
-node('master') {
+node('slave') {
     unarchive mapping: ['target/x.war' : 'x.war']
-    sh "mvn cargo:redeploy -Dhost=prod.jetty.docker"
-    echo 'Deployed to http://prod.jetty.docker'
-}
-
-def deploy(war, id) {
-    sh "cp ${war} /tmp/webapps/${id}.war"
-}
-
-def undeploy(id) {
-    sh "rm /tmp/webapps/${id}.war"
-}
-
-def runWithServer(body) {
-    def id = UUID.randomUUID().toString()
-    deploy 'target/x.war', id
-    try {
-        body.call "http://localhost:8080/${id}/"
-    } finally {
-        undeploy id
-    }
+    sh "mvn cargo:redeploy -Dhost=${prodHost}"
+    echo 'Deployed to http://${prodHost}:8080'
 }
