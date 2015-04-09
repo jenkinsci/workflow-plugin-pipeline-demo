4c97a2e56abf8c30f9718fcee8fb84fb66c7a671
Kurt Madel <kmadel@mac.com>
2015-04-09 19:06:21 -0400

    updated mvn tool

--- flow.groovy
+++ flow.groovy
@@ -14,19 +14,16 @@ node('slave') {
     }, quickerTests: {
         sh "mvn -f sometests/pom.xml test -Durl=http://${hostDev}:8080/demo-war/ -Dduration=20"
     })
+    checkpoint('Passed tests, ready to deploy to ${hostStage}')
     stage name: 'Staging', concurrency: 1
     sh "mvn cargo:redeploy -Dhost=${hostStage}"
 }
 
-input message: "Does ${hostStage} look good?"
-try {
-    checkpoint('Before production')
-} catch (NoSuchMethodError _) {
-    echo 'Checkpoint feature available in Jenkins Enterprise by CloudBees.'
-}
+input message: "Does http://${hostStage}:8080/demo-war/ look good?"
+checkpoint('Before production deployment')
 stage name: 'Production', concurrency: 1
 node('slave') {
     unarchive mapping: ['target/x.war' : 'x.war']
     sh "mvn cargo:redeploy -Dhost=${hostProd}"
-    echo 'Deployed to ${hostProd}'
+    echo 'Deployed to http://${hostProd}:8080/demo-war/'
 }
